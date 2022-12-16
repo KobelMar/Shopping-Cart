810a2a0b3d4756a514461fee3fef4684a9a0d894
KobelMar <max.marcisiak@gmail.com>
2022-12-16 14:34:05 +0100

    .

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,9 +6,6 @@ import CartItem from "./CartItem";
 
 const Cart = (props) => {
   const cartItems = useSelector((state) => state.cart.items);
-
-  console.log(cartItems);
-
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
